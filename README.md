4aafdd7e5ffa5204609fd422c7eea74bd68f359f
slinkydeveloper <francescoguard@gmail.com>
2022-05-11 10:42:50 +0200

    Feedback

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ E2E tests for Restate
 * `functions` contains a collection of functions for e2e testing
 * `test-utils` contains utilities to develop e2e tests
 * `tests` contains the test code
+
+## Setup local env
+
+To run locally, you need to setup two env variables to download artifacts from Github Packages:
+
+```bash
+export GITHUB_ACTOR=<yourusername>
+export GITHUB_TOKEN=<PAM>
+```
+
+To create a PAM check https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
---
+++ functions/counter/impl/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    java
+    idea
+    alias(libs.plugins.shadowJar)
+    alias(libs.plugins.spotless)
+    alias(libs.plugins.errorprone)
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.restate.sdk)
+    implementation(project(":functions:counter:contract"))
+
+    implementation(libs.protobuf.java)
+    implementation(libs.grpc.stub)
+    implementation(libs.grpc.protobuf)
+
+    implementation(libs.log4j.api)
+    implementation(libs.log4j.core)
+
+    implementation(libs.grpc.netty.shaded)
+
+    errorprone(libs.errorprone)
+}
+
+// Use gradle shadowJar to build the fat jar
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "dev.restate.e2e.functions.counter.Main"
+    }
+}
