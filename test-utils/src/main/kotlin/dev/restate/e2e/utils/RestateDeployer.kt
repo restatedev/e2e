afbb4737b74340f021c073ef454e697eefcb52e0
slinkydeveloper <francescoguard@gmail.com>
2022-05-19 16:12:08 +0200

    Now the test should pass

--- test-utils/src/main/kotlin/dev/restate/e2e/utils/RestateDeployer.kt
+++ test-utils/src/main/kotlin/dev/restate/e2e/utils/RestateDeployer.kt
@@ -3,6 +3,7 @@ package dev.restate.e2e.utils
 import org.junit.jupiter.api.fail
 import org.slf4j.LoggerFactory
 import org.testcontainers.containers.GenericContainer
+import org.testcontainers.containers.Network
 import org.testcontainers.containers.output.Slf4jLogConsumer
 import org.testcontainers.utility.DockerImageName
 import org.testcontainers.utility.MountableFile
@@ -10,7 +11,7 @@ import java.io.File
 import java.net.URL
 import java.nio.file.Files
 
-class RestateDeployer private constructor(functions: List<FunctionContainer>) {
+class RestateDeployer private constructor(val runtimeDeployments: Int, functions: List<FunctionContainer>) {
 
     companion object {
         private const val RUNTIME_CONTAINER = "ghcr.io/restatedev/runtime:main"
@@ -23,9 +24,8 @@ class RestateDeployer private constructor(functions: List<FunctionContainer>) {
     private var runtimeContainer: GenericContainer<*>? = null
 
     init {
-        if (functionContainers.size != 1) {
-            fail("At the moment you MUST specify exactly one function container")
-        }
+        assert(functionContainers.size == 1) {"At the moment you MUST specify exactly one function container"}
+        assert(runtimeDeployments == 1) { "At the moment only one runtime deployment is supported" }
     }
 
     data class FunctionContainer(val name: String) {
@@ -33,25 +33,33 @@ class RestateDeployer private constructor(functions: List<FunctionContainer>) {
             return GenericContainer(
                 DockerImageName.parse("localhost/restatedev/$name")
             )
-                .withExposedPorts(8080)
                 .withEnv("PORT", "8080")
                 .withLogConsumer(Slf4jLogConsumer(logger))
         }
     }
 
-    data class Builder(var functions: MutableList<FunctionContainer> = mutableListOf()) {
+    data class Builder(var runtimeDeployments: Int = 1, var functions: MutableList<FunctionContainer> = mutableListOf()) {
 
         fun function(name: String) = apply { this.functions.add(FunctionContainer(name)) }
 
         fun function(functionContainer: FunctionContainer) = apply { this.functions.add(functionContainer) }
 
-        fun build() = RestateDeployer(functions)
+        fun runtimeDeployments(runtimeDeployments: Int) = apply {
+            this.runtimeDeployments = runtimeDeployments
+        }
+
+        fun build() = RestateDeployer(runtimeDeployments, functions)
     }
 
     fun deploy() {
+        val network = Network.newNetwork()
+
         // Deploy functions
         functionContainers.forEach { (name, genericContainer) ->
-            genericContainer.start()
+            genericContainer
+                .withNetwork(network)
+                .withNetworkAliases(name)
+                .start()
             logger.debug("Started function container {} with endpoint {}", name, getFunctionEndpointUrl(name))
         }
 
@@ -73,13 +81,14 @@ class RestateDeployer private constructor(functions: List<FunctionContainer>) {
         logger.debug("Written config to {}", configFile)
 
         // Generate runtime container
-
         runtimeContainer = GenericContainer(
             DockerImageName.parse(RUNTIME_CONTAINER)
         )
             .dependsOn(functionContainers.values)
             .withEnv("RUST_LOG", "debug")
             .withExposedPorts(RUNTIME_GRPC_ENTRYPOINT)
+            .withNetwork(network)
+            .withNetworkAliases("runtime")
             .withLogConsumer(Slf4jLogConsumer(logger))
             .withCopyFileToContainer(MountableFile.forHostPath(configFile), "/restate.yaml")
             .withCommand("--id 1 --configuration-file /restate.yaml")
@@ -93,9 +102,7 @@ class RestateDeployer private constructor(functions: List<FunctionContainer>) {
     }
 
     fun getFunctionEndpointUrl(name: String): URL {
-        val funcContainer =
-            functionContainers[name] ?: throw java.lang.IllegalStateException("Function does not exists")
-        return URL("http", "127.0.0.1", funcContainer.getMappedPort(8080), "/")
+        return URL("http", name, 8080, "/")
     }
 
     fun getRuntimeFunctionEndpointUrl(name: String): URL {
--- tests/src/test/kotlin/dev/restate/e2e/CounterTest.kt
+++ tests/src/test/kotlin/dev/restate/e2e/CounterTest.kt
@@ -4,6 +4,9 @@ import dev.restate.e2e.functions.counter.CounterGrpc.CounterBlockingStub
 import dev.restate.e2e.utils.RestateDeployer
 import dev.restate.e2e.utils.RestateDeployerExtension
 import dev.restate.e2e.utils.RestateDeployerExtension.InjectBlockingStub
+import io.grpc.Metadata
+import io.grpc.stub.MetadataUtils
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.RegisterExtension
 
@@ -19,6 +22,23 @@ class CounterTest {
     @Test
     fun test(@InjectBlockingStub("e2e-counter") counterClient: CounterBlockingStub) {
         counterClient.add(dev.restate.e2e.functions.counter.Number.newBuilder().setValue(1).build())
+    }
+
+    @Test
+    fun testKeyed(@InjectBlockingStub("e2e-counter") counterClient: CounterBlockingStub) {
+        // TODO move the stub key logic to the extension?
+        val meta = Metadata()
+        meta.put(Metadata.Key.of("x-restate-id", Metadata.ASCII_STRING_MARSHALLER), "my-key")
+        val counterClient = counterClient
+            .withInterceptors(MetadataUtils.newAttachHeadersInterceptor(meta))
+
+        val res1 = counterClient.getAndAdd(dev.restate.e2e.functions.counter.Number.newBuilder().setValue(1).build())
+        assertThat(res1.oldValue).isEqualTo(0);
+        assertThat(res1.newValue).isEqualTo(1);
+
+        val res2 = counterClient.getAndAdd(dev.restate.e2e.functions.counter.Number.newBuilder().setValue(2).build())
+        assertThat(res2.oldValue).isEqualTo(1);
+        assertThat(res2.newValue).isEqualTo(3);
     }
 
 }
