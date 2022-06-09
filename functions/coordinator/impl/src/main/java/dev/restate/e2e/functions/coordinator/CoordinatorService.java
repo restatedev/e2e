8aa8a9373eb7dd12605010a67f6c70e58c9726f0
Till Rohrmann <till@restate.dev>
2022-06-09 15:29:06 +0200

    Update CoordinatorService to work with latest java-sdk

--- functions/coordinator/impl/src/main/java/dev/restate/e2e/functions/coordinator/CoordinatorService.java
+++ functions/coordinator/impl/src/main/java/dev/restate/e2e/functions/coordinator/CoordinatorService.java
@@ -13,8 +13,7 @@ public class CoordinatorService extends CoordinatorGrpc.CoordinatorImplBase {
   public void sleep(Duration request, StreamObserver<Empty> responseObserver) {
     RestateContext ctx = RestateContext.current();
 
-    LOG.info(
-        "Putting service {} to sleep for {} ms", ctx.id().orElse("unknown"), request.getMillis());
+    LOG.info("Putting service {} to sleep for {} ms", ctx.key(), request.getMillis());
 
     ctx.sleep(java.time.Duration.ofMillis(request.getMillis()));
 
