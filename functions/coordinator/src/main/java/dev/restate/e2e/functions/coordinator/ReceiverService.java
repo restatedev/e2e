fd1e9477c8235fef6b1c1515e93d4be65f5de6c2
slinkydeveloper <francescoguard@gmail.com>
2023-04-06 16:17:22 +0200

    Convert coordinator service

--- functions/coordinator/src/main/java/dev/restate/e2e/functions/coordinator/ReceiverService.java
+++ functions/coordinator/src/main/java/dev/restate/e2e/functions/coordinator/ReceiverService.java
@@ -1,14 +1,17 @@
 package dev.restate.e2e.functions.coordinator;
 
 import com.google.protobuf.Empty;
-import dev.restate.e2e.functions.receiver.*;
-import dev.restate.sdk.RestateContext;
-import dev.restate.sdk.StateKey;
+import dev.restate.e2e.functions.receiver.ReceiverGrpc;
+import dev.restate.e2e.functions.receiver.ReceiverProto.*;
+import dev.restate.sdk.blocking.RestateBlockingService;
+import dev.restate.sdk.core.StateKey;
+import dev.restate.sdk.core.TypeTag;
 import io.grpc.stub.StreamObserver;
 
-public class ReceiverService extends ReceiverGrpc.ReceiverImplBase {
+public class ReceiverService extends ReceiverGrpc.ReceiverImplBase
+    implements RestateBlockingService {
 
-  public static final StateKey<String> STATE_KEY = StateKey.of("my-state", String.class);
+  public static final StateKey<String> STATE_KEY = StateKey.of("my-state", TypeTag.STRING_UTF8);
 
   @Override
   public void ping(PingRequest request, StreamObserver<Pong> responseObserver) {
@@ -18,9 +21,7 @@ public class ReceiverService extends ReceiverGrpc.ReceiverImplBase {
 
   @Override
   public void setValue(SetValueRequest request, StreamObserver<Empty> responseObserver) {
-    var ctx = RestateContext.current();
-
-    ctx.set(STATE_KEY, request.getValue());
+    restateContext().set(STATE_KEY, request.getValue());
 
     responseObserver.onNext(Empty.getDefaultInstance());
     responseObserver.onCompleted();
@@ -28,9 +29,7 @@ public class ReceiverService extends ReceiverGrpc.ReceiverImplBase {
 
   @Override
   public void getValue(GetValueRequest request, StreamObserver<GetValueResponse> responseObserver) {
-    var ctx = RestateContext.current();
-
-    var state = ctx.get(STATE_KEY).orElse("");
+    var state = restateContext().get(STATE_KEY).orElse("");
 
     responseObserver.onNext(GetValueResponse.newBuilder().setValue(state).build());
     responseObserver.onCompleted();
