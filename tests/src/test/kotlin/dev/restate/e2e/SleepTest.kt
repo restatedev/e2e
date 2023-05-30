04ea80055db77cb79171d062628ad3d93831caae
Till Rohrmann <till@restate.dev>
2023-05-30 19:00:18 +0200

    Reduce timer and parallelism

--- tests/src/test/kotlin/dev/restate/e2e/SleepTest.kt
+++ tests/src/test/kotlin/dev/restate/e2e/SleepTest.kt
@@ -76,11 +76,11 @@ abstract class BaseSimpleSleepTest {
 
   @RepeatedTest(10)
   fun manySleeps(@InjectChannel runtimeChannel: Channel) =
-      runTest(timeout = 20.seconds) {
+      runTest(timeout = 60.seconds) {
         val minSleepDuration = 100.milliseconds
         val maxSleepDuration = 500.milliseconds
-        val sleepsPerInvocation = 20
-        val concurrentSleepInvocations = 50
+        val sleepsPerInvocation = 10
+        val concurrentSleepInvocations = 10
 
         val coordinatorClient = CoordinatorGrpcKt.CoordinatorCoroutineStub(runtimeChannel)
 
