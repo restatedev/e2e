2c3f4cf8d34202e341ab120647d7c9924f455543
Giselle van Dongen <giselle.vd@gmail.com>
2023-07-12 14:04:38 +0200

    Fix broken side effect test for node (#154)

--- tests/src/test/kotlin/dev/restate/e2e/SideEffectTest.kt
+++ tests/src/test/kotlin/dev/restate/e2e/SideEffectTest.kt
@@ -7,7 +7,6 @@ import dev.restate.e2e.utils.InjectBlockingStub
 import dev.restate.e2e.utils.RestateDeployer
 import dev.restate.e2e.utils.RestateDeployerExtension
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
@@ -15,8 +14,11 @@ import org.junit.jupiter.api.extension.RegisterExtension
 import org.junit.jupiter.api.parallel.Execution
 import org.junit.jupiter.api.parallel.ExecutionMode
 
+// Note: Java SDK only waits for the ack upon the next blocking operation.
+// So the last execution will have 1 as result because the last side effect will get executed and
+// then the response gets send back immediately.
 @Tag("only-always-suspending")
-class JavaSideEffectTest : BaseSideEffectTest() {
+class JavaSideEffectTest : BaseSideEffectTest(1) {
   companion object {
     @RegisterExtension
     val deployerExt: RestateDeployerExtension =
@@ -30,9 +32,12 @@ class JavaSideEffectTest : BaseSideEffectTest() {
   }
 }
 
+// Note: TS SDK awaits the ack after each side effect.
+// So the last execution will have 0 as result because the last side effect will lead to a
+// suspension and a replay.
+// During the last replay no side effects will be executed anymore.
 @Tag("only-always-suspending")
-@Disabled("https://github.com/restatedev/sdk-typescript/issues/82")
-class NodeSideEffectTest : BaseSideEffectTest() {
+class NodeSideEffectTest : BaseSideEffectTest(0) {
   companion object {
     @RegisterExtension
     val deployerExt: RestateDeployerExtension =
@@ -46,7 +51,7 @@ class NodeSideEffectTest : BaseSideEffectTest() {
   }
 }
 
-abstract class BaseSideEffectTest {
+abstract class BaseSideEffectTest(private val sideEffectExecutionCountExpectedValue: Int) {
   @DisplayName("Side effect should wait on acknowledgements")
   @Test
   @Execution(ExecutionMode.CONCURRENT)
@@ -55,6 +60,6 @@ abstract class BaseSideEffectTest {
             sideEffectStub.invokeSideEffects(
                 SideEffectProto.InvokeSideEffectsRequest.newBuilder().build()))
         .extracting { it.nonDeterministicInvocationCount }
-        .isEqualTo(1)
+        .isEqualTo(sideEffectExecutionCountExpectedValue)
   }
 }
