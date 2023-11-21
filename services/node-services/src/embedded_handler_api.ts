60eb8f0f3affc1ac6ad2b513e07df444921484b2
Jack Kleeman <jackkleeman@gmail.com>
2023-11-21 16:10:37 +0000

    Add license headers

--- services/node-services/src/embedded_handler_api.ts
+++ services/node-services/src/embedded_handler_api.ts
@@ -1,3 +1,12 @@
+// Copyright (c) 2023 - Restate Software, Inc., Restate GmbH
+//
+// This file is part of the Restate e2e tests,
+// which are released under the MIT license.
+//
+// You can find a copy of the license in file LICENSE in the root
+// directory of this repository or package, or at
+// https://github.com/restatedev/e2e/blob/main/LICENSE
+
 import express, { Request, Response } from "express";
 import * as restate from "@restatedev/restate-sdk";
 import { CounterAPI } from "./handler_api";
--- tests/src/test/kotlin/dev/restate/e2e/Containers.kt
+++ tests/src/test/kotlin/dev/restate/e2e/Containers.kt
@@ -1,3 +1,12 @@
+// Copyright (c) 2023 - Restate Software, Inc., Restate GmbH
+//
+// This file is part of the Restate e2e tests,
+// which are released under the MIT license.
+//
+// You can find a copy of the license in file LICENSE in the root
+// directory of this repository or package, or at
+// https://github.com/restatedev/e2e/blob/main/LICENSE
+
 package dev.restate.e2e
 
 import dev.restate.e2e.services.collections.list.ListServiceGrpc
