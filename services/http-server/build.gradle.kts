404641f187267bea7580fdeb6581acf105046c91
Jack Kleeman <jackkleeman@gmail.com>
2023-11-22 17:17:55 +0000

    Check license headers on .kt files (#220)
    * Check license headers on .kt files
    
    And check on CI
    
    * Workdir

--- services/http-server/build.gradle.kts
+++ services/http-server/build.gradle.kts
@@ -1,3 +1,12 @@
+// Copyright (c) 2023 - Restate Software, Inc., Restate GmbH
+//
+// This file is part of the Restate e2e tests,
+// which are released under the MIT license.
+//
+// You can find a copy of the license in file LICENSE in the root
+// directory of this repository or package, or at
+// https://github.com/restatedev/e2e/blob/main/LICENSE
+
 import dev.restate.e2e.gradle.util.hostArchitecture
 import dev.restate.e2e.gradle.util.testBaseImage
 
