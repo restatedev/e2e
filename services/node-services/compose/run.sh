f839d358184aab08a5ccefb8dba50e8cc1aecfdf
igalshilman <igal@restate.dev>
2024-04-08 16:09:45 +0200

    [interpreter] Use ENV variaible instead of stdin for the InterpreterJob
    This commit only effects the interpreter job (not the server) and makes
    it possible to pass in the test configuration via the env variable
    called INTERPRETER_DRIVER_CONF.
    This works better with docker and docker-compose.

---
+++ services/node-services/compose/run.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+SEED=$(date --iso-8601=seconds)
+TIMEOUT_SECNODS=3600
+
+export INTERPRETER_DRIVER_CONF=$(cat <<-EOF 
+			{
+        "seed" : "${SEED}", 
+        "keys" : 100,
+        "tests" : 100,
+        "maxProgramSize" : 15,
+        "ingress" : "http://restate:8080",
+        "register" : {
+          "adminUrl" : "http://restate:9070",
+          "deployments" : ["http://interpreter:9080"]
+        }
+      }
+EOF
+)
+
+
+docker-compose -f compose.template.yml up \
+	--abort-on-container-exit \
+	--exit-code-from driver \
+	--force-recreate \
+	--timeout ${TIMEOUT_SECNODS}
+
+
