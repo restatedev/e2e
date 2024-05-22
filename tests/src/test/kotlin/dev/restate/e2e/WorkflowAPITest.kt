5556e800a9be681b666df0497641cf2a4eb5700f
igalshilman <igal@restate.dev>
2024-05-22 11:13:11 +0200

    [node] Disable Workflow test temporarily

--- tests/src/test/kotlin/dev/restate/e2e/WorkflowAPITest.kt
+++ tests/src/test/kotlin/dev/restate/e2e/WorkflowAPITest.kt
@@ -19,6 +19,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.awaitility.kotlin.await
 import org.awaitility.kotlin.matches
 import org.awaitility.kotlin.untilCallTo
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
@@ -76,6 +77,7 @@ class NodeWorkflowAPITest {
   @Test
   @DisplayName("Set and resolve durable promise leads to completion")
   @Execution(ExecutionMode.CONCURRENT)
+  @Disabled("While suspension tests are resolved")
   fun setAndResolve(@InjectIngressClient ingressClient: IngressClient) {
     val client =
         WorkflowAPIBlockAndWaitClient.fromIngress(ingressClient, UUID.randomUUID().toString())
