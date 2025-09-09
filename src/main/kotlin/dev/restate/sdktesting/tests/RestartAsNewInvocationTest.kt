1edbe8d2ebdf3e6749f0c05ea203a71b39014683
slinkydeveloper <me@slinkydeveloper.com>
2025-09-09 12:40:51 +0200

    Add test for restart from prefix

--- src/main/kotlin/dev/restate/sdktesting/tests/RestartAsNewInvocationTest.kt
+++ src/main/kotlin/dev/restate/sdktesting/tests/RestartAsNewInvocationTest.kt
@@ -33,6 +33,7 @@ import org.awaitility.kotlin.await
 import org.awaitility.kotlin.withAlias
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.RegisterExtension
+import java.util.concurrent.atomic.AtomicReference
 
 class RestartAsNewInvocationTest {
 
@@ -40,17 +41,20 @@ class RestartAsNewInvocationTest {
   class RestartInvocation {
     companion object {
       val shouldFail = AtomicBoolean(true)
+      val ctxRunResult = AtomicReference("old")
     }
 
     @Handler
     suspend fun echo(ctx: Context, input: String): String {
+      val from = ctx.runBlock { ctxRunResult.get() }
+
       // Load if we should fail
       val shouldFail = shouldFail.get()
       if (shouldFail) {
         throw TerminalException("Sorry, can't make any progress")
       }
 
-      return input
+      return "$input from $from"
     }
   }
 
@@ -68,6 +72,7 @@ class RestartAsNewInvocationTest {
   ) = runTest {
     // First attempt should fail
     RestartInvocation.shouldFail.set(true)
+    RestartInvocation.ctxRunResult.set("old")
 
     val input = "my-input"
 
@@ -87,17 +92,18 @@ class RestartAsNewInvocationTest {
 
     // Next attempt must not fail
     RestartInvocation.shouldFail.set(false)
+    RestartInvocation.ctxRunResult.set("new")
 
     // Now restart invocation
     val adminClient = ApiClient().setHost(adminURI.host).setPort(adminURI.port)
     val invocationApi = InvocationApi(adminClient)
     val newInvocationId =
-        invocationApi.restartAsNewInvocation(sendResult.invocationId()).newInvocationId
+        invocationApi.restartAsNewInvocation(sendResult.invocationId(), null, null).newInvocationId
 
     // Assert this returns the input
     val newInvocationResult =
         ingressClient.invocationHandle(newInvocationId, String::class.java).attachSuspend()
-    assertThat(newInvocationResult.response).isEqualTo(input)
+    assertThat(newInvocationResult.response).isEqualTo("$input from new")
 
     // Assert we both got invocation status completed
     await withAlias
@@ -111,6 +117,61 @@ class RestartAsNewInvocationTest {
               .isEqualTo(
                   SysInvocationEntry(id = newInvocationId, status = "completed"),
               )
+        }
+  }
+
+  @Test
+  fun restartFromPrefix(
+    @InjectClient ingressClient: Client,
+    @InjectAdminURI adminURI: URI,
+  ) = runTest {
+    // First attempt should fail
+    RestartInvocation.shouldFail.set(true)
+    RestartInvocation.ctxRunResult.set("old")
+
+    val input = "my-input"
+
+    // Create clients for the services
+    val restartClient = RestartAsNewInvocationTestRestartInvocationClient.fromClient(ingressClient)
+
+    // Send request first time
+    val sendResult = restartClient.send().echo(input, init = idempotentCallOptions)
+    val initialInvocationId = sendResult.invocationId()
+
+    // Should fail with terminal exception
+    assertThat(runCatching { sendResult.attach() })
+      .extracting { it.exceptionOrNull() }
+      .asInstanceOf(type(IngressException::class.java))
+      .extracting({ it.message }, STRING)
+      .contains("Sorry, can't make any progress")
+
+    // Next attempt must not fail
+    RestartInvocation.shouldFail.set(false)
+    RestartInvocation.ctxRunResult.set("new")
+
+    // Now restart invocation
+    val adminClient = ApiClient().setHost(adminURI.host).setPort(adminURI.port)
+    val invocationApi = InvocationApi(adminClient)
+    val newInvocationId =
+      invocationApi.restartAsNewInvocation(sendResult.invocationId(), 1, null).newInvocationId
+
+    // Assert this returns the input
+    val newInvocationResult =
+      ingressClient.invocationHandle(newInvocationId, String::class.java).attachSuspend()
+    assertThat(newInvocationResult.response).isEqualTo("$input from old")
+
+    // Assert we both got invocation status completed
+    await withAlias
+        "got the two invocation status completed" untilAsserted
+        {
+          assertThat(getInvocationStatus(adminURI, initialInvocationId))
+            .isEqualTo(
+              SysInvocationEntry(id = initialInvocationId, status = "completed"),
+            )
+          assertThat(getInvocationStatus(adminURI, newInvocationId))
+            .isEqualTo(
+              SysInvocationEntry(id = newInvocationId, status = "completed"),
+            )
         }
   }
 }
